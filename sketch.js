0171a38c08efde74b2c293640339d213163a4a40
paytonimeyer <32025639+paytonimeyer@users.noreply.github.com>
2017-10-23 20:16:18 -0400

    Push 2

--- sketch.js
+++ sketch.js
@@ -1,9 +1,11 @@
-
+var speedX = 1
+var r = 0;
+var diameter = 50;
 
 
 function setup() {
 
-	createCanvas(500,500);
+	createCanvas(1000,1000);
 	background(153);
 
 }
@@ -12,6 +14,9 @@ function draw() {
 
 	fill(255);
 	noStroke();
-	ellipse(width/2,height/2, 50, 50);
+	ellipse(width/2 , r+ random(1,3), diameter, diameter);
+
+	r = r + speedX;
+	diameter = diameter - .05;
 
 }
